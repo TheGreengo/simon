c31901ae38da646b55cd733cc2f486951c281929
Braden <bradenoptimus@gmail.com>
2023-02-28 22:07:55 -0700

    scores functionality implemented

---
+++ scores.js
@@ -0,0 +1,35 @@
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+  
+    const tableBody = document.querySelector('#scores');
+  
+    if (scores.length) {
+      for (const [i, score] of scores.entries()) {
+        const position = document.createElement('td');
+        const name = document.createElement('td');
+        const score = document.createElement('td');
+        const date = document.createElement('td');
+  
+        position.textContent = i + 1;
+        name.textContent = score.name;
+        score.textContent = score.score;
+        date.textContent = score.date;
+  
+        const row = document.createElement('tr');
+        row.appendChild(positionTdEl);
+        row.appendChild(nameTdEl);
+        row.appendChild(scoreTdEl);
+        row.appendChild(dateTdEl);
+  
+        tableBody.appendChild(row);
+      }
+    } else {
+      tableBody.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+    }
+  }
+  
+  loadScores();
